6825233fee6c141a35ef76a85bd7b3b8d4a19a26
Philip L. Wolper <phi.wolper@tum.de>
2023-05-23 18:51:05 +0200

    ploting tajD in genomic regions

--- code/TajD-plots.R
+++ code/TajD-plots.R
@@ -1,17 +1,51 @@
 library(tidyverse)
 library(here)
 
-TajD.chr2 <- read.csv(file = here("output/Pf7_Kenya_2014_Taj.D_pos500000-1000000.txt"), sep = "\t")
+## Reading in data
+TajD.chr2 <- read.csv(file = here("output/TajD/data/Pf7_Kenya_2014_Taj.D_pos500000-1000000.txt"), sep = "\t")
 str(TajD.chr2)
 
-Pfsa1 <- 631190
-Pfsa2 <- 814288
+TajD.chr2.Pfsa12-region.Kenya_Gambia <- read.csv(file = here("output/TajD/data/Pf7_Kenya_Gambia_incl.unknowns.txt"), sep = "\t")
+str(TajD.chr2.Pfsa12-region.Kenya_Gambia)
 
-# Plotting Taj-D
-TajD.plot <- ggplot(data = subset(TajD.chr2,!is.na(pop.1)), aes(x=position, y = pop.1)) +
-  geom_line() +
-  geom_vline(xintercept = Pfsa1, colour = 'red') +
-  geom_vline(xintercept = Pfsa2, colour = 'blue')
+TajD.chr2.Kenya_Gambia <- read.csv(file = here("output/TajD/data/Pf7_Kenya_Gambia_whole_chr2_incl.unknown.txt"), sep = "\t")
+str(TajD.chr2.Kenya_Gambia)
 
-ggsave(TajD.plot, file = here("output/Pf7_Kenya_2014_Taj.D_pos500000-1000000.png"),
-       device = "png", dpi = 300)
+## Formatting data for plotting
+format_Popgenome_TajD <- function(TajD, populations){
+  # TajD: Popgenome array output; populations: vector of population identifiers.
+  positions <- TajD$position
+  TajD <- subset(TajD, select = -c(position))
+  colnames(TajD) <- populations
+  TajD_total <- unname(unlist(TajD))
+  df <- data.frame(population = rep(populations, each = nrow(TajD)), TajD = TajD_total)
+  return(cbind(positions, df))
+}
+
+
+Kenya_Gambia <- format_Popgenome_TajD(TajD.chr2.Kenya_Gambia, c("Kenya", "Gambia"))
+
+# Loci of interest
+Pfsa1 <- 631190/1000000
+Pfsa2 <- 814288/1000000
+
+# Plotting Taj-D function
+
+plot_TajD <- function(TajD_formatted, fileName){
+  TajD.plot <- ggplot(data = subset(TajD_formatted,!is.na(TajD)), aes(x=positions/1000000, y = TajD)) +
+    geom_line(aes(color = population)) +
+    geom_vline(xintercept = Pfsa1) +
+    geom_text(aes(x=Pfsa1, label="Pfsa1", y=1), angle=90, vjust = 1.2) +
+    geom_vline(xintercept = Pfsa2) +
+    geom_text(aes(x=Pfsa2, label="Pfsa2", y=1), angle=90, vjust = 1.2) +
+    theme_light() +
+    labs(title = "Tajima's D calculated in a genomic region of Chr 2 of P. falciparum",
+         subtitle = "Sliding window of 10 Kb in length",
+         x = "Genomic position in Mb")
+
+  ggsave(TajD.plot, file = here("output/TajD/png/",fileName),
+         device = "png", dpi = 300,
+         width = 14, height = 7)
+}
+
+plot_TajD(Kenya_Gambia, "Pf7.chr2_Kenya_Gambia_TajD.png")
