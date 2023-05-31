edc553f58c5750f284746180afcb7f8c9fed13bc
pwolper <philip.wolper@gmail.com>
2023-05-31 19:52:57 +0200

    added DRC samples

--- code/Pf7-samples.R
+++ code/Pf7-samples.R
@@ -25,8 +25,8 @@ unique(Pf7_samples$Admin.level.1[Pf7_samples$Country == "Kenya"])
 Pf7_samples_Kenya <- Pf7_samples[Pf7_samples$Country == "Kenya" &
                                  Pf7_samples$Year %in% c(2010,2011,2012,2013,2014) &
                                  Pf7_samples$QC.pass == "True" ,]
-Pf7_samples_Kenya_2014 %>% str()
-Pf7_samples_Kenya_2014 %>% summary()
+Pf7_samples_Kenya %>% str()
+Pf7_samples_Kenya %>% summary()
 
 
 Pf7_samples_Gambia <- Pf7_samples[Pf7_samples$Country == "Gambia" &
@@ -35,16 +35,27 @@ Pf7_samples_Gambia <- Pf7_samples[Pf7_samples$Country == "Gambia" &
 Pf7_samples_Gambia_UpperRiver %>% str()
 Pf7_samples_Gambia_UpperRiver %>% summary()
 
-Pf7_samples_DRC
-##
+Pf7_samples_DRC <- Pf7_samples[Pf7_samples$Country == "Democratic Republic of the Congo" &
+                               Pf7_samples$QC.pass == "True",]
+Pf7_samples_DRC %>% str()
+Pf7_samples_DRC %>% summary()
+
+Pf7_samples_Malawi <- Pf7_samples[]
+
+
 samples_as_dataframe <- function(samples,Name){
-  df <- data.frame(id = samples, Country = rep(Name, times = length(samples)))
+  df <- data.frame(id = samples$Sample,
+                   Country = rep(Name, times = nrow(samples)),
+                   year = samples$Year)
   return(df)
 }
 
 Kenya <- samples_as_dataframe(Pf7_samples_Kenya,"Kenya")
 Gambia <- samples_as_dataframe(Pf7_samples_Gambia_UpperRiver,"Gambia")
+DRC <- samples_as_dataframe(Pf7_samples_DRC, "DRC")
 
-Pf7_multi_samples <- rbind(Kenya, Gambia)
+Pf7_multi_samples <- rbind(Kenya, Gambia, DRC)
+Pf7_multi_samples %>% str()
+Pf7_multi_samples %>% summary()
 
 write.table(Pf7_multi_samples, file = "data/Pf7/sample_ids/Pf7_multi_samples.txt", sep = "\t", row.names = FALSE, quote=FALSE)
