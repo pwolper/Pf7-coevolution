1fe5cb68dc573accb62547475437570c8d3009e2
pwolper <philip.wolper@gmail.com>
2023-05-31 19:17:01 +0200

    file movements mainly

--- code/Pf7-samples.R
+++ code/Pf7-samples.R
@@ -2,7 +2,7 @@
 
 library("tidyverse")
 library(here)
-setwd("/data/home/students/p.wolper/FP-coevolution/")
+# setwd("/data/home/students/p.wolper/FP-coevolution/")
 here()
 
 Pf7_samples <- read.csv(file="data/Pf7/sample_ids/Pf7_samples-full-info.txt", sep = "\t")
@@ -11,7 +11,6 @@ Pf7_samples %>% str()
 names(Pf7_samples);unique(Pf7_samples$Population)
 
 Pf7 <- subset(Pf7_samples,select = c("Sample", "Country", "Year", "Population", "Sample.type", "Admin.level.1", "QC.pass"))
-Pf7 %>% str()
 
 write.table(Pf7, file="data/Pf7/sample_ids/Pf7_samples.txt", sep = "\t", row.names = FALSE)
 
@@ -23,20 +22,20 @@ Pf7_samples %>% str()
 unique(Pf7_samples$Admin.level.1[Pf7_samples$Country == "Gambia"])
 unique(Pf7_samples$Admin.level.1[Pf7_samples$Country == "Kenya"])
 
-Pf7_samples_Kenya_2014 <- Pf7_samples[Pf7_samples$Country == "Kenya" &
-                                             Pf7_samples$Year == 2014 &
-                                             Pf7_samples$QC.pass == "True" &
-                                             Pf7_samples$Admin.level.1 == "Kisumu",]
+Pf7_samples_Kenya <- Pf7_samples[Pf7_samples$Country == "Kenya" &
+                                 Pf7_samples$Year %in% c(2010,2011,2012,2013,2014) &
+                                 Pf7_samples$QC.pass == "True" ,]
 Pf7_samples_Kenya_2014 %>% str()
 Pf7_samples_Kenya_2014 %>% summary()
 
 
-Pf7_samples_Gambia_UpperRiver <- Pf7_samples[Pf7_samples$Country == "Gambia" &
+Pf7_samples_Gambia <- Pf7_samples[Pf7_samples$Country == "Gambia" &
                                              Pf7_samples$QC.pass == "True" &
                                              Pf7_samples$Admin.level.1 == "Upper River",]
 Pf7_samples_Gambia_UpperRiver %>% str()
 Pf7_samples_Gambia_UpperRiver %>% summary()
 
+Pf7_samples_DRC
 ##
 samples_as_dataframe <- function(samples,Name){
   df <- data.frame(id = samples, Country = rep(Name, times = length(samples)))
