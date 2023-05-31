bd36bac59b0a94f59a09ba982ada15715a2aa9da
pwolper <philip.wolper@gmail.com>
2023-05-31 08:03:48 +0200

    qsub-bcftools

--- code/qsub-bcftools/qsub-bcftools-stats.sh
+++ code/qsub-bcftools/qsub-bcftools-stats.sh
@@ -8,14 +8,14 @@
 # Specify this option only for multithreaded jobs that use more than one cpu core.
 # IMPORTANT! Don't use more than 4 cores to keep free space for other students!
 
-#$-l vf=1g
+#$-l vf=2g
 # This option declares the amount of memory the job will use. Specify this value as accurate as possible.
 # IMPORTANT! memory request is multiplied by the number of requested CPU cores specified with the -pe.
 # Thus, you should divide the overall memory consumption of your job by the number of parallel threads.
 
 #$-j yes
 
-#$-q praktikum
+#$-q all.q
 #$-N vcf-snp-stats
 
 source ~/.bashrc
@@ -26,9 +26,8 @@ output=$HOME/FP-coevolution/output
 echo STARTED on $(date)
 
 ## bcftools
-bcftools stats $vcf_dir/Pf3D7_02_v3.qSNP.vcf.gz > $output/vcf.stats/Pf3D7_02_v3.qSNP.stats
+bcftools stats $vcf_dir/Pf3D7_11_v3.SNP.vcf.gz > $output/vcf.stats/Pf3D7_11_v3.SNP.stats
 
 echo FINISHED writing output file on $(date)
-
 
 echo FINISHED task on$(date)
