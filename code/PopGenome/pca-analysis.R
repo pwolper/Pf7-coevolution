6fac65264949570ac3018aacb5cfa680a9c5cd13
Philip L. Wolper <phi.wolper@tum.de>
2023-06-08 16:23:07 +0200

    pca analysis

--- code/PopGenome/pca-analysis.R
+++ code/PopGenome/pca-analysis.R
@@ -1,6 +1,8 @@
 ## R script for PCA analysis of the Pf7 populations
 library(here)
 library(tidyverse)
+library(parallel)
+
 if(!require(adegenet)){
     install.packages("adegenet")
     library(adegenet)}
@@ -18,7 +20,7 @@ Pf7.metadata <- read.csv(here("data/Pf7/sample_ids/Pf7_multi_samples.txt"), sep 
 
 Pf7.snp <- read.vcfR(vcf_path, verbose = TRUE)
 
-Pf7.snp.gl <- vcfR2genlight(Pf7.snp)
+Pf7.snp.gl <- vcfR2genlight(Pf7.snp, n.cores = 8) #add n.cores for parallelization
 
 pop(Pf7.snp.gl) <- Pf7.metadata$Country
 ploidy(Pf7.snp.gl) <- 1
@@ -28,20 +30,26 @@ Pf7.snp.gl
 Pf7.snp.gl@pop
 
 ## Step 2: Calculating PCA using adegenet::glPca(). How do the populations cluster? By geographic location? Can we see effects of the years?
-Pf7.pca <- glPca(Pf7.snp.gl, nf = 10)
+Pf7.pca <- glPca(Pf7.snp.gl, nf = 10, parallel = TRUE, n.cores = 8) #add parallel=TRUE, n.cores for parallelization
 Pf7.pca
 
-Pf7.pca.scores <- as_tibble(pca.scores.Pf7$scores)
+Pf7.pca.scores <- as_tibble(Pf7.pca$scores)
 Pf7.pca.scores$Country <- Pf7.metadata$Country
+Pf7.pca.scores$Year <- Pf7.metadata$year
+Pf7.pca.scores$id <- Pf7.metadata$id
 Pf7.pca.scores
 
 ## Extracting eigenvalues
 eig.total <- sum(Pf7.pca$eig)
 
+write.table(here("output/pca/Pf7.pca.eigenvalues.txt"), eig.total , sep = "\t")
+write.table(here("output/pca/Pf7.pca.scores.txt"), Pf7.pca.scores, sep = "\t")
+write.table(here("output/pca/Pf7.pca.loadings.txt"), Pf7.pca$loadings, sep = "\t")
+
 ## Plotting the pca
 Pf7.plot12 <- ggplot(Pf7.pca.scores, aes(PC1,PC2)) + geom_point()
 
 
-ggsave(Pf7.plot12, file = here("output/pca/","Pf7.chr2.pca.samples.png"),
+ggsave(Pf7.plot12, file = here("output/pca/","Pf7.chr2.pca.parallel.samples.png"),
          device = "png", dpi = 300,
          width = 7, height = 7)
