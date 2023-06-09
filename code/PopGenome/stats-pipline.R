af559a154d683f50a7c6bfd346d9502f07a05c65
Philip L. Wolper <phi.wolper@tum.de>
2023-06-09 16:45:36 +0200

    popgen stats calculations new

---
+++ code/PopGenome/stats-pipline.R
@@ -0,0 +1,45 @@
+## Calculation of theta_W for the sliding windows of Pf7 chromosomes.
+##
+
+library(tidyverse)
+library(PopGenome)
+library(here)
+
+## Variables
+filename <- "Pf3D7_02_v3.samples.SNP.vcf.gz"
+data_dir <- "chr2" #vcf subfolder contaning the vcf data
+chr_tid <- "Pf3D7_02_v3"
+chr_start <- 1
+chr_end <- 1000000#2040000 for chr11
+
+output_filename <- "Pf7.chr2.stats.DRC_GM_KE_MM"
+output_txt <- paste0(output_filename,".txt")
+output_png <- paste0(output_filename,".png")
+
+#### CODE STARTS HERE ####
+## Read Sample data
+Pf7_samples <- read.csv(here("data/Pf7/sample_ids/Pf7_multi_samples.txt"), sep = "\t")
+
+## Reading in the vcf data
+source(here("code/PopGenome/functions/read_Pf7_vcf.R"))
+vcf <- read_Pf7_vcf(dir = data_dir, file = filename, chr_tid, Pf7_samples, chr_start, chr_end)
+
+## Set Populations
+source(here("code/PopGenome/functions/set_populations.R"))
+vcf <- set_populations(vcf, Pf7_samples, population = Pf7_samples$Country)
+
+# Neutrality stats
+vcf.neutrality <- neutrality.stats(vcf, FAST=TRUE)
+
+get.neutrality(vcf.neutrality, theta = TRUE)[[1]]
+
+# Segregating sites on sliding window
+source(here("code/PopGenome/functions/calc_stats_sliding_window.R"))
+sliding.stats <- calc_stats_sliding_window(vcf, 10000, 2500)
+
+
+# Plotting the segregating sites
+source(here("code/PopGenome/functions/TajD-plots.R"))
+sliding.stats.formatted <- format_Popgenome_stats(sliding.stats, output_txt)
+
+plot_S_chr11(vcf.S.formatted,output_png)
