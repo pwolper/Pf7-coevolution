3d2087dd28f77992c8dc59e200cfdab747bd4d73
Philip Wolper <philip.wolper@gmail.com>
2023-06-10 10:47:15 +0200

    pca plot improve

---
+++ code/PopGenome/pca-plots.R
@@ -0,0 +1,18 @@
+## R script for plotting the results if the pca-analysis script
+
+library(tidyverse)
+library(here)
+
+## Load Files 
+pca.scores <- read.csv(here("output/pca/Pf7.pca.scores.txt"), sep = "\t")
+pca.eigenvalues <- read.csv(here("output/pca/Pf7.pca.eigenvalues.txt"), sep = "\t")
+pca.loading <- read.csv(here("output/pca/Pf7.pca.loadings.txt"), sep = "\t")
+
+
+## Plotting the pca
+Pf7.plot12 <- ggplot(Pf7.pca.scores, aes(PC1,PC2)) + geom_point(aes(col=Country, shape = Year))
+
+
+ggsave(Pf7.plot12, file = here("output/pca/","Pf7.chr2.pca.png"),
+         device = "png", dpi = 300,
+         width = 7, height = 7)
