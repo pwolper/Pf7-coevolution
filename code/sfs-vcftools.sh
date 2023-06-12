f8fd16271d0c598a6eb681643730597597bdfc58
pwolper <philip.wolper@gmail.com>
2023-06-12 14:06:14 +0200

    vcftools snp freq

---
+++ code/sfs-vcftools.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+vcftools --gzvcf ../data/Pf7/vcf/chr2/Pf3D7_02_v3.afr_samples.SNP.vcf.gz --freq --out afr_freq
--- exec/sfs-vcftools-exec.sh
+++ exec/sfs-vcftools-exec.sh
@@ -4,19 +4,19 @@
 # This option tells gridware to change to the current directory before executing the job
 # (default is the home of the user).
 
-#$-pe serial 1
+#$-pe serial 8
 # Specify this option only for multithreaded jobs that use more than one cpu core.
 # IMPORTANT! Don't use more than 4 cores to keep free space for other students!
 
-#$-l vf=1g
+#$-l vf=4g
 # This option declares the amount of memory the job will use. Specify this value as accurate as possible.
 # IMPORTANT! memory request is multiplied by the number of requested CPU cores specified with the -pe.
 # Thus, you should divide the overall memory consumption of your job by the number of parallel threads.
 
 #$-j yes
 
-#$-q bigmem@node09
-#$-N download-data
+#$-q bigmem@node09.pgen.wzw.tum.de
+#$-N freq-snp
 
 source ~/.bashrc
 
