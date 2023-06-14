ebbc836e510272f4e5934c8d9e5db03c4a276160
Philip L. Wolper <phi.wolper@tum.de>
2023-06-14 16:41:22 +0200

    minor changes

--- code/downloading/ftp_download.sh
+++ code/downloading/ftp_download.sh
@@ -3,3 +3,7 @@
 #preliminary ftp download script
 
 wget  "ftp://ngs.sanger.ac.uk/production/malaria/Resource/34/Pf7_vcf/"
+
+# P.reichenowi Download, this is the embl file
+wget ftp://ftp.sanger.ac.uk/pub/project/pathogens/Plasmodium/reichenowi/2018/October_2018/PrCDC_02_v3.embl.gz
+wget ftp://ftp.sanger.ac.uk/pub/project/pathogens/Plasmodium/reichenowi/2018/October_2018/PrCDC_11_v3.embl.gz
