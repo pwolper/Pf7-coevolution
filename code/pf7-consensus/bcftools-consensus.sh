1e86603ff7689cfffc8999b5c8996b975fff3245
Philip L. Wolper <phi.wolper@tum.de>
2023-06-29 09:59:23 +0200

    consensus code

--- code/pf7-consensus/bcftools-consensus.sh
+++ code/pf7-consensus/bcftools-consensus.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Input file names
-vcf_file="multisample.vcf"
-reference_file="reference.fasta"
+vcf_file=$HOME/FP-coevolution/data/Pf7/vcf/chr2/Pf3D7_02_v3.afr_samples.SNP.vcf.gz
+reference_file=$HOME/FP-coevolution/data/seqs/Pf3D7_02_v3.fasta
+consensus_output=$HOME/FP-coevolution/data/Pf7/consensus_sequences
 
 # Create a directory to store the output consensus sequences
-mkdir -p consensus_sequences
+mkdir -p $consensus_output 
+cd $consensus_output
+
+# Reference header
+ref_header=$(awk '/>/ {print; exit}' "$reference_file")
 
 # Get a list of sample names from the VCF file
 sample_names=$(bcftools query -l "$vcf_file")
