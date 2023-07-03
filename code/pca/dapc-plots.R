8aca88f78e3c508712536afd5967a54b2fd02642
Philip L. Wolper <phi.wolper@tum.de>
2023-07-03 16:42:23 +0200

    dapc

---
+++ code/pca/dapc-plots.R
@@ -0,0 +1,24 @@
+## R script that visualizes the results of the dapc analysis
+## 22.06.2023, Philip Wolper
+library(here)
+library(tidyverse)
+library(adegenet)
+
+rds_file <- here("output/pca/Pf7_chr11_dapc.rds")
+
+outfile <- here("output/pca/Pf7.chr11.dapc.african")
+outfile.txt <- paste0(outfile, ".txt")
+outfile.png <- paste0(outfile, ".png")
+
+## Read in the rst file saved in dapc-analysis.R
+dapc <- readRDS(rds_file)
+
+dapc$prior %>% str()
+
+png(outfile.png)
+
+scatter(dapc, cell = 0, pch = 18:23, cstar = 0, lwd = 2, lty = 2, legend = TRUE, scree.pca = TRUE)
+
+dev.off()
+
+## Choose indivduals from each population for the further analysis with MMC
