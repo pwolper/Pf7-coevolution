4c47edca465e133ca7a5ecb123dcadc3d92177b4
Philip L. Wolper <phi.wolper@tum.de>
2023-08-03 17:39:45 +0200

    minor

--- code/qsub-bcftools/bcftools_SNP.sh
+++ code/qsub-bcftools/bcftools_SNP.sh
@@ -9,14 +9,14 @@ echo STARTED on $(date)
 ## bcftools
 bcftools view \
     --include 'FILTER="PASS" && N_ALT=1 && TYPE="snp" && VQSLOD> 5.0'\
-    --samples-file $samples_dir/Pf7_african_samples_ids.txt \
+    --samples-file $samples_dir/Pf7_african_samples_2014_ids.txt \
     --output-type z\
-    --output-file  $vcf_dir/Pf3D7_02_v3.afr_samples.qSNP.vcf.gz \
-    $vcf_dir/Pf3D7_02_v3.pf7.vcf.gz
+    --output-file  $vcf_dir/Pf3D7_02_v3.afr_samples_2014.qSNP.vcf.gz \
+    $vcf_dir/Pf3D7_02_v3.afr_samples.qSNP.vcf.gz
 
 echo FINISHED writing output file on $(date)
 
-bcftools index -t $vcf_dir/Pf3D7_02_v3.afr_samples.qSNP.vcf.gz
+bcftools index -t $vcf_dir/Pf3D7_02_v3.afr_samples_2014.qSNP.vcf.gz
 
 echo FINISHED indexing on $(date)
 echo FINISHED task on$(date)
