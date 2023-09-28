7aa7616f148e08bf19eac99e9117b77060720056
pwolper <philip.wolper@gmail.com>
2023-09-28 14:24:59 +0200

    unfolded sfs

--- code/sfs/unfolded/plot_sfs.R
+++ code/sfs/unfolded/plot_sfs.R
@@ -1,11 +1,21 @@
+library(here); library(ggplot2); library(dplyr)
+
+vcf_repol <- read.csv(here("output/sfs/unfolded/Pf7.chr2.snps.polarized.txt"))
+vcf_repol$SF <- vcf_repol$AF*1846
+
+round(vcf_repol$SF,0) %>% max()
+
+str(vcf_repol)
+
+
 
 png(here("output/sfs/unfolded/sfs_unfolded_chr2_afr_samples.png"), height = 500, width = 1000)
 ## plot(names(unfolded_sfs_vcf_repol), unfolded_sfs_vcf_repol, type = "h",
 ##      xlab = "Unfolded frequency", ylab = "Count",
 ##      main = "Unfolded site frequency for Pf7 chr2 snps polarized with P. reichenowi")
 
-hist(vcf_repol_filtered$AF, breaks = 300, ylim = c(0,1000), xlab = "Unfolded frequency", ylab = "Count",
+hist(vcf_repol$SF, breaks = 1846, xlim = c(0,50), xlab = "Unfolded frequency", ylab = "Count",
      main = "Unfolded site frequency for Pf7 chr2 snps polarized with P. reichenowi",
-     sub = paste(nrow(vcf_repol_filtered),"sites from 1846 individuals"))
+     sub = paste(nrow(vcf_repol),"sites from 1846 individuals"))
 
 dev.off()
